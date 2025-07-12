1a93230cba0fd739c87bc94756385a08d4836116
benchagraa <benchagra.badr@gmail.com>
2025-07-12 17:23:28 +0200

    Add project files.

---
+++ FinanceProject/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace FinanceProject.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Content { get; set; } = String.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        public Stock? Stock { get; set; }
+    }
+}
